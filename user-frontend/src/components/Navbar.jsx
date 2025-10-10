b2af458b5938077d22aeee8e99eb09f3aea44f0a
Chinmay Mehrotra <mehrotrachinmay6@gmail.com>
2025-10-11 00:21:00 +0530

    bg of cart

--- user-frontend/src/components/Navbar.jsx
+++ user-frontend/src/components/Navbar.jsx
@@ -133,7 +133,7 @@ function Navbar(){
 
             {/* Right Side - Search and Cart */}
             <div className="flex items-center space-x-1">
-            <button className="p-2 rounded-full hover:bg-gray-100 transition-colors hover:cursor-pointer" onClick={()=>setSearchQuery(!searchQuery)}>
+            <button className="p-2 rounded-full hover:bg-[#95C5F4] transition-colors hover:cursor-pointer" onClick={()=>setSearchQuery(!searchQuery)}>
             <svg className="w-5 h-5 text-gray-700" fill="none" stroke="currentColor" viewBox="0 0 24 24">
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
             </svg>
