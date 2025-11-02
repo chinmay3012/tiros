231044704b571e74a6b50339bc0307928bd4a265
Chinmay Mehrotra <mehrotrachinmay6@gmail.com>
2025-11-03 01:30:05 +0530

    grid commit

--- user-frontend/src/pages/HomePage.jsx
+++ user-frontend/src/pages/HomePage.jsx
@@ -140,7 +140,7 @@ function HomePage(){
                                 </button>
                             </>
                         )}
-                        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+                        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-6">
                             {visibleProducts.map((p) => (
                                 <div key={p._id}>
                                     <ProductsCard 
